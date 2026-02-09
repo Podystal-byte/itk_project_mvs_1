adc81f8d158c971b112162ad0629c4f12d3ebd41
istri <vani4848@yandex.ru>
2026-02-09 06:26:24 +0300

    Initial commit

---
+++ src/main/java/org/example/OrdersAndUsersApps.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersAndUsersApps {
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersAndUsersApps.class);
+    }
+}
